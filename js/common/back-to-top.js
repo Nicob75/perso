395ba1e91a835d10029cd6e18936bdf25b1a92e4
Nicob75 <nicolas.barthelemy75@gmail.com>
2019-12-02 13:02:00 +0100

    -folder structure : removed the extra site/ part

--- js/common/back-to-top.js
+++ js/common/back-to-top.js
@@ -3,12 +3,12 @@ function btn_back_to_top(pTopSection) {
 	if ( $(window).scrollTop() > amountScrolled ) {
         // TODO change picture to up arrow !
         // $('a.back-to-top').children('img').fadeTo('slow', 1.0);
-        $('a.back-to-top').children('img').attr('src', '../../img/site/common/icons/up_arrow.png');
+        $('a.back-to-top').children('img').attr('src', '../../img/common/icons/up_arrow.png');
         $('a.back-to-top').attr('href', '#page-top');
     } else {
         // TODO change picture to down arrow !
         // $('a.back-to-top').children('img').fadeTo('slow', 0.75);
-        $('a.back-to-top').children('img').attr('src', '../../img/site/common/icons/down_arrow.png');
+        $('a.back-to-top').children('img').attr('src', '../../img/common/icons/down_arrow.png');
         $('a.back-to-top').attr('href', pTopSection);
     }
 }
